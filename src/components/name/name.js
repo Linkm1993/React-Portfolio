ceb6a1197a8919a94d8f853b61212ccc4c2f5566
Matt Link <Linkm1993@gmail.com>
2020-06-14 21:45:37 -0400

    Added name compoent, name is now rendering

---
+++ src/components/name/name.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function firstName() {
+    return (
+    <div>
+        <div className="d-flex justify-content-start left">
+            <div className="name">Matthew</div>
+        </div>
+            <div className="d-flex justify-content-start left">
+            <div className="name">Link</div>
+        </div>
+    </div>    
+    )
+}
